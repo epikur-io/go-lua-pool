ec94a384dab2b955bc715329f6c8a354a0d8d0bb
Epikur <3p1kur@protonmail.ch>
2025-02-24 13:54:54 +0100

    go-lua VM pool

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/epikur-io/go-lua-pool
+
+go 1.22.3
+
+require github.com/epikur-io/go-lua v0.0.0-20250224085517-17039441b5fb
---
+++ pool.go
@@ -0,0 +1,98 @@
+package pool
+
+import (
+	"errors"
+	"time"
+
+	lua "github.com/epikur-io/go-lua"
+)
+
+func NewPool(size int) *Pool {
+	lp := Pool{Size: size}
+	return &lp
+}
+
+type Pool struct {
+	// Size of the pool
+	Size int
+	// Custructor function to create Lua VMs
+	Creator func() *lua.State
+	pool    chan *lua.State
+}
+
+func (p *Pool) init() {
+	p.pool = make(chan *lua.State, p.Size)
+	// fill the pool
+	for i := 0; i < p.Size; i++ {
+		var lvm *lua.State
+		if p.Creator != nil {
+			lvm = p.Creator()
+		} else {
+			lvm = NewLuaVM()
+		}
+		p.pool <- lvm
+	}
+}
+
+func (p *Pool) Len() int {
+	return len(p.pool)
+}
+
+func (p *Pool) Cap() int {
+	return cap(p.pool)
+}
+
+func (p *Pool) Update() {
+	// Make sure the pool is empty so we don't miss a vm because
+	// it was acquired by an other function
+	// So this loop can take a while if some vm's are already acquired and busy.
+	for i := 0; i < cap(p.pool); i++ {
+		// empty the Pool
+		<-p.pool
+	}
+	for i := 0; i < cap(p.pool); i++ {
+		// fill the Pool
+		var lvm *lua.State
+		if p.Creator != nil {
+			lvm = p.Creator()
+		} else {
+			lvm = NewLuaVM()
+		}
+		p.pool <- lvm
+	}
+}
+
+func (p *Pool) AcquireTimeout(to time.Duration) (*lua.State, error) {
+	c := time.After(to)
+	select {
+	case vm := <-p.pool:
+		return vm, nil
+	case <-c:
+		return nil, errors.New("timeout")
+	}
+}
+
+func (p *Pool) Acquire() *lua.State {
+	return <-p.pool
+}
+
+func (p *Pool) Release(vm *lua.State) {
+	if vm == nil {
+		var lvm *lua.State
+		if p.Creator != nil {
+			lvm = p.Creator()
+		} else {
+			lvm = NewLuaVM()
+		}
+		p.pool <- lvm
+		return
+	}
+	p.pool <- vm
+}
+
+// Default constructor to create Lua VMs
+func NewLuaVM() *lua.State {
+	lvm := lua.NewState()
+	lua.OpenLibraries(lvm)
+	return lvm
+}
